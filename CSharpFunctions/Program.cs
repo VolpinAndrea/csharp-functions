313d8ade09f8e92be99c24535cd392a288d40764
VolpinAndrea <116800862+VolpinAndrea@users.noreply.github.com>
2022-11-24 15:10:36 +0100

    StampaInteri

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,1 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int[] numeri = { 12, 23, 34, 56, 12 };
+StampaArrayInteri(numeri);
+
+//-------FUNZIONI
+void StampaArrayInteri(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write(array[i]);
+        }
+        else
+        {
+            Console.Write(array[i] + ",");
+        }
+    }
+
+    Console.WriteLine("]");
+}
